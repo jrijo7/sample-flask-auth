a641c9489dc74b080fc190a34217be754549559e
João Rijo <joaovictor_gomes.7@hotmail.com>
2025-12-02 20:19:02 -0300

    Adding read_user function

--- app.py
+++ app.py
@@ -1,50 +1,99 @@
 from flask import Flask, request, jsonify
 from models.user import User
 from database import db
-from flask_login import LoginManager, login_user, current_user, logout_user, login_required # Importa LoginManager para gerenciar sessões de usuário
+from flask_login import (
+    LoginManager,
+    login_user,
+    current_user,
+    logout_user,
+    login_required,
+)  # Importa LoginManager para gerenciar sessões de usuário
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "your_secret_key" # chave secreta usada para proteger sessões e formulários
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db" #caminho que o sqlalchemy vai usar para criar o banco de dados
+app.config["SECRET_KEY"] = (
+    "your_secret_key"  # chave secreta usada para proteger sessões e formulários
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    "sqlite:///database.db"  # caminho que o sqlalchemy vai usar para criar o banco de dados
+)
 
-login_manager = LoginManager() # instancia do LoginManager
-db.init_app(app) # inicializa a extensão do banco de dados com a aplicação Flask
-login_manager.init_app(app) # inicializa a extensão do LoginManager com a aplicação Flask
+login_manager = LoginManager()  # instancia do LoginManager
+db.init_app(app)  # inicializa a extensão do banco de dados com a aplicação Flask
+login_manager.init_app(
+    app
+)  # inicializa a extensão do LoginManager com a aplicação Flask
 
 # view login
-login_manager.login_view = 'login' # define a rota de login
+login_manager.login_view = "login"  # define a rota de login
 
-#Session <- Conexão ativa
+
+# Session <- Conexão ativa
 @login_manager.user_loader
 def load_user(user_id):
-  return User.query.get(int(user_id)) # consulta o usuário pelo ID
+    return User.query.get(int(user_id))  # consulta o usuário pelo ID
 
-@app.route('/login', methods = ['POST'])
+
+@app.route("/login", methods=["POST"])
 def login():
-  data = request.get_json()
-  username = data.get("username")
-  password = data.get("password")
-  
-  if username and password:
-    # Login
-    user = User.query.filter_by(username=username).first()
-    
-    if user and user.password == password:
-      login_user(user) # faz o login do usuário
-      print(current_user.is_authenticated) # verifica se o usuário está autenticado
-      return jsonify({'message': 'Login successful'})
-  
-  return jsonify({'message': 'Invalid credentials'}), 400
+    data = request.get_json()
+    username = data.get("username")
+    password = data.get("password")
 
-@app.route('/logout', methods=['GET'])
+    if username and password:
+        # Login
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.password == password:
+            login_user(user)  # faz o login do usuário
+            print(
+                current_user.is_authenticated
+            )  # verifica se o usuário está autenticado
+            return jsonify({"message": "Login successful"})
+
+    return jsonify({"message": "Invalid credentials"}), 400
+
+
+@app.route("/logout", methods=["GET"])
 @login_required
 def logout():
     logout_user()
-    return jsonify({'message': 'Logout successful'})
+    return jsonify({"message": "Logout successful"})
 
-@app.route('/hello-world', methods=['GET'])
+
+@app.route("/user", methods=["POST"])
+@login_required
+def create_user():
+    data = request.get_json()
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+      user = User(username=username, password=password)
+      db.session.add(user)
+      db.session.commit()
+      return jsonify({"message": "User created successfully"})
+      
+    return jsonify({"message": "Dados inválidos"}), 401
+
+@app.route("/user/<int:id_user>", methods=["GET"])
+@login_required
+def read_user(id_user):
+  user = User.query.get(id_user)
+  if user:
+    return {"username": user.username}
+  return jsonify({"message": "User not found"}), 404
+
+@app.route("/user/<int:user_id>", methods=["GET"])
+def read_user(user_id):
+    user = User.query.get(user_id)
+    if user:
+        return {"username": user.username}
+    return jsonify({"message": "User not found"}), 404
+
+@app.route("/hello-world", methods=["GET"])
 def hello_world():
-    return 'Hello, World!'
-  
-if __name__ == '__main__':
-  app.run(debug=True)
+    return "Hello, World!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
